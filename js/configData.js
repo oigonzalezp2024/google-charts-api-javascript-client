755c19b9444e2ae434b0a4a0d60167971b9edca4
Oscar Gonzalez <oigonzalezp2024@gmail.com>
2024-04-30 15:33:39 -0500

    HTML limpio.

---
+++ js/configData.js
@@ -0,0 +1,129 @@
+
+export const configData = [
+    {
+        "data": [
+            ['Mushrooms', 1],
+            ['Onions', 2],
+            ['Olives', 3],
+            ['Zucchini', 5],
+            ['Pepperoni', 8]
+        ],
+        "options": {
+            'title': 'Preferencias en Bogotá',
+            'width': 400,
+            'height': 300
+        },
+        "columns": [
+            ['string', 'Topping'],
+            ['number', 'Slices']
+        ]
+    },
+    {
+        "data": [
+            ['Mushrooms', 13],
+            ['Onions', 21],
+            ['Olives', 33],
+            ['Zucchini', 52],
+            ['Pepperoni', 85]
+        ],
+        "options": {
+            'title': 'Preferencias en Medellin',
+            'width': 400,
+            'height': 300
+        },
+        "columns": [
+            ['string', 'Topping'],
+            ['number', 'Slices']
+        ]
+    },
+    {
+        "data": [
+            ['Mushrooms', 52],
+            ['Onions', 33],
+            ['Olives', 13],
+            ['Zucchini', 8],
+            ['Pepperoni', 5]
+        ],
+        "options": {
+            'title': 'Preferencias en Bucaramanga',
+            'width': 400,
+            'height': 300
+        },
+        "columns": [
+            ['string', 'Topping'],
+            ['number', 'Slices']
+        ]
+    },
+    {
+        "data": [
+            ['Mushrooms', 3],
+            ['Onions', 2],
+            ['Olives', 1],
+            ['Zucchini', 2],
+            ['Pepperoni', 3]
+        ],
+        "options": {
+            'title': 'Preferencias en Pereira',
+            'width': 400,
+            'height': 300
+        },
+        "columns": [
+            ['string', 'Topping'],
+            ['number', 'Slices']
+        ]
+    },
+    {
+        "data": [
+            ['Mushrooms', 5],
+            ['Onions', 8],
+            ['Olives', 13],
+            ['Zucchini', 21],
+            ['Pepperoni', 33]
+        ],
+        "options": {
+            'title': 'Preferencias en Barranquilla',
+            'width': 400,
+            'height': 300
+        },
+        "columns": [
+            ['string', 'Topping'],
+            ['number', 'Slices']
+        ]
+    },
+    {
+        "data": [
+            ['Mushrooms', 21],
+            ['Onions', 13],
+            ['Olives', 8],
+            ['Zucchini', 5],
+            ['Pepperoni', 3]
+        ],
+        "options": {
+            'title': 'Preferencias en Cali',
+            'width': 400,
+            'height': 300
+        },
+        "columns": [
+            ['string', 'Topping'],
+            ['number', 'Slices']
+        ]
+    },
+    {
+        "data": [
+            ['Mushrooms', 5],
+            ['Onions', 8],
+            ['Olives', 13],
+            ['Zucchini', 21],
+            ['Pepperoni', 33]
+        ],
+        "options": {
+            'title': 'Preferencias en Cúcuta',
+            'width': 400,
+            'height': 300
+        },
+        "columns": [
+            ['string', 'Topping'],
+            ['number', 'Slices']
+        ]
+    }
+]
